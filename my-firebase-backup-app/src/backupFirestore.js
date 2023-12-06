3f45ca5786a238d733440a2d01794e0150157f2f
Trần Thị Trà Giang <54837897+kami2608@users.noreply.github.com>
2023-12-06 17:31:14 +0700

    backup authentication data

--- my-firebase-backup-app/src/backupFirestore.js
+++ my-firebase-backup-app/src/backupFirestore.js
@@ -1,26 +1,57 @@
+// const admin = require("firebase-admin");
+
+// const serviceAccount = require("./serviceAccountKey.json"); // đường dẫn đến file này trong thư mục, có thể để như này và thay đổi lại nội dung trong file cũng được
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://upload-file-demo-d1beb.firebaseio.com", // Thay thế bằng URL của dự án Firebase, có dạng: https://<projectId>.firebaseio.com, chỗ <projectID> thì thay bằng id của dự án trong firebase của mình
+// });
+
+// const db = admin.firestore();
+// const backupData = {};
+
+// db.collection("users")
+//   .get()
+//   .then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//       backupData[doc.id] = doc.data();
+//     });
+
+//     const fs = require("fs");
+//     // có thể đổi tên file backup.json, muốn lưu vào file nào thì đổi tên tương ứng.
+//     fs.writeFileSync("backup.json", JSON.stringify(backupData, null, 2));
+//     console.log("Sao lưu dữ liệu thành công.");
+//   })
+//   .catch((error) => {
+//     console.error("Lỗi khi sao lưu dữ liệu:", error);
+//   });
+
+
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json"); // đường dẫn đến file này trong thư mục, có thể để như này và thay đổi lại nội dung trong file cũng được
+const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://upload-file-demo-d1beb.firebaseio.com", // Thay thế bằng URL của dự án Firebase, có dạng: https://<projectId>.firebaseio.com, chỗ <projectID> thì thay bằng id của dự án trong firebase của mình
+  databaseURL: "https://upload-file-demo-d1beb.firebaseio.com", // Thay thế bằng URL của dự án
 });
 
-const db = admin.firestore();
+const auth = admin.auth();
 const backupData = {};
 
-db.collection("users")
-  .get()
-  .then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      backupData[doc.id] = doc.data();
+auth.listUsers()
+  .then((listUsersResult) => {
+    listUsersResult.users.forEach((userRecord) => {
+      // Trích xuất thông tin xác thực của người dùng và lưu vào backupData
+      backupData[userRecord.uid] = {
+        email: userRecord.email,
+        // Thêm các thông tin khác mà bạn muốn sao lưu
+      };
     });
 
     const fs = require("fs");
-    // có thể đổi tên file backup.json, muốn lưu vào file nào thì đổi tên tương ứng.
-    fs.writeFileSync("backup.json", JSON.stringify(backupData, null, 2));
-    console.log("Sao lưu dữ liệu thành công.");
+    // Lưu dữ liệu sao lưu vào tệp JSON, có thể đổi tên tệp theo ý muốn.
+    fs.writeFileSync("authBackup.json", JSON.stringify(backupData, null, 2));
+    console.log("Sao lưu thông tin xác thực thành công.");
   })
   .catch((error) => {
-    console.error("Lỗi khi sao lưu dữ liệu:", error);
+    console.error("Lỗi khi sao lưu thông tin xác thực:", error);
   });
